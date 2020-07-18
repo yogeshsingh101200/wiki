b6817b9ec657e41ec876e1bc2ab153d8926c8602
yogeshsingh <yogeshsingh101200@gmail.com>
2020-07-18 22:02:08 +0530

    now displays entries which now are links that can be visited to display specific entry and enables search for entries

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,7 +1,12 @@
+""" Contains url-view mapping for encylopedia app """
+
 from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:title>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,39 @@
+""" Contains views for encylopedia app """
+
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
 
 def index(request):
+    """ default route """
+    if request.method == "POST":
+        form_data = request.POST
+        query = form_data.get("q")
+        entries = util.list_entries()
+        similar = []
+        for a_entry in entries:
+            if query == a_entry:
+                return HttpResponseRedirect(reverse("encyclopedia:entry", args=(a_entry,)))
+            if query in a_entry:
+                similar.append(a_entry)
+        return render(request, "encyclopedia/index.html", {
+            "entries": similar
+        })
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    """ NIL """
+    entry_content = util.get_entry(title)
+    if entry_content is None:
+        raise Http404
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": entry_content
+    })
