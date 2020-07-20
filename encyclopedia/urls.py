c82744e48c9c55a8d263f46cf136c6384ae5bc1e
yogeshsingh <yogeshsingh101200@gmail.com>
2020-07-20 17:42:46 +0530

    mapped randompage method to randompage path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("create", views.create, name="create"),
-    path("edit/<str:title>", views.edit, name="edit")
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("randompage", views.randompage, name="randompage")
 ]
